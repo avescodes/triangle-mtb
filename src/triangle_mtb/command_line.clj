8e215b319dd12b43423c8a0c33cd7fa4af6da385
Ryan Neufeld <ryan@ryanneufeld.ca>
2012-10-20 13:13:53 -0400

    Include some printouts in command line runner

--- src/triangle_mtb/command_line.clj
+++ src/triangle_mtb/command_line.clj
@@ -1,9 +1,11 @@
 (ns triangle-mtb.command-line
-  (:require [triangle-mtb.trail-status :as mtb])
-  (:require [triangle-mtb.datomic :as trails])
+  (:require [triangle-mtb.trail-status :as mtb]
+            [triangle-mtb.datomic :as trails])
   (:gen-class :main true))
 
 (defn -main
   "Triangle MTB Command Line Tool"
   [& args]
-  (trails/update-trail-status (mtb/current-status)))
+  (println "Updating trail statuses...")
+  (trails/update-trail-status (mtb/current-status))
+  (println "Updated trail statuses."))
